20406143b01b4607e141b2ca06cd709a08819bae
stefan <stefanbogdanovic030@gmail.com>
2019-01-21 13:40:31 +0100

    Root query initalization

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 // Set up middleware
 // Fire graphqlHTTP everytime we hit endpoint /graphql, we use it as middleware here and it will handle graphQL request
 app.use('/graphql', graphqlHTTP({
-
+    schema: schema
 }));
 
 app.listen(4000, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,14 @@
 // Describe Schema on Graph (describes object types, relations between objects and how we can reach into the graph)
+/*
+* Three responsibilities:
+*  1. Define types
+*  2. Define relationships between types
+*  3. Define route queries
+*
+* */
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -12,4 +19,23 @@ const BookType = new GraphQLObjectType({
     })
 });
 
-module.exports(BookType);
+const RootQuery = new GraphQLObjectType({
+   name: 'RootQueryType',
+   fields: {
+       book: {
+           // Query for BookType
+           type: BookType,
+           // Expect client to pass some arguments along
+           args: { id: {type: GraphQLString}},
+           // in reslove we write code to get whichever data we need from our db or some other source
+           resolve(parent, args) {
+               args.id
+           }
+       }
+   }
+});
+
+module.exports = new GraphQLSchema({
+   // Initial route query
+   query: RootQuery
+});
